3995aae4152f75f945f96d0b51b98eb5ace70069
Sergey Ryabov <56312515+s3kkt@users.noreply.github.com>
2023-05-10 02:58:21 +0400

    Revert "add telegram bot, some SQL improvements && small fixes (#1)"
    This reverts commit 6053f8c3b4df9bfd05d6f681077bd600c3fc1fae.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,19 +2,12 @@ package config
 
 import (
 	"flag"
-	"fmt"
 	"github.com/s3kkt/github-releases-bot/internal"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 	"regexp"
 )
-
-func ParseFlags() string {
-	configPath := flag.String("config", "/", "path to YAML config file")
-	flag.Parse()
-	return *configPath
-}
 
 func GetConfig(configPath string) internal.Config {
 	config, err := os.ReadFile(configPath)
@@ -29,13 +22,14 @@ func GetConfig(configPath string) internal.Config {
 	return c
 }
 
+func ParseFlags() string {
+	configPath := flag.String("config", "/", "path to YAML config file")
+	flag.Parse()
+	return *configPath
+	//return "./configs/config.yml"
+}
+
 func GetApiURL(url string) string {
 	re := regexp.MustCompile(`github.com/`)
 	return re.ReplaceAllString(url, `api.github.com/repos/`) + "/releases/latest"
-}
-
-func DatabaseConnectionString(configPath string) string {
-	conf := GetConfig(configPath)
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
-		conf.Database.Username, conf.Database.Password, conf.Database.Host, conf.Database.Port, conf.Database.DBName)
 }
--- internal/transport/github-api.go
+++ internal/transport/github-api.go
@@ -2,46 +2,38 @@ package transport
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"github.com/s3kkt/github-releases-bot/internal"
 	"io"
 	"log"
 	"net/http"
 )
 
-func GetReleases(repoUrl string, token string) (internal.Release, error) {
+func GetReleases(repoUrl string, token string) internal.Release {
 	client := &http.Client{}
-	release := internal.Release{}
-	apiError := internal.APIError{}
-
 	req, err := http.NewRequest("GET", repoUrl, nil)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 	req.Header.Set("Authorization", "Bearer "+token)
 	res, err := client.Do(req)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
-
+	var release internal.Release
 	err = json.Unmarshal(body, &release)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
-	if release.HtmlUrl == "" {
-		err = json.Unmarshal(body, &apiError)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err := errors.New("Error recieving latest release for " + repoUrl + " Message: " + apiError.Message)
-		return release, err
-	} else {
-		return release, nil
-	}
+	return release
 }
